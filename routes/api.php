3384e5bc60c2be02f295bd18b5b67267c622c9a2
latifi777 <abdelalilatifi3@gmail.com>
2025-03-19 10:29:00 +0000

    la correction de route

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resourceapi('articles', ArticleController::class);
+Route::apiResource('articles', ArticleController::class);
